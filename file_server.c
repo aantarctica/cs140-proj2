b716073cbae424d7d70c20126d11cad4a862dd3f
Mutya Gulapa <mcgulapa@up.edu.ph>
2022-01-14 01:19:23 +0800

    Added locks, rng, sleep, and command queue

--- file_server.c
+++ file_server.c
@@ -4,14 +4,22 @@
 #include <string.h>
 #include <time.h>
 #include <pthread.h>
-// #include <semaphore.h>
+#include <unistd.h>
+#include <semaphore.h>
 
 typedef struct CMD command;
 struct CMD {
     char type[10];
     char dir[51];
     char str[51];
+    command* nextcmd;
 };  
+
+typedef struct QUEUE cmdqueue;
+struct QUEUE {
+    command* head;
+    command* tail;
+};
 
 // function initializations
 command* parsecmd(char buffer[150]);
@@ -19,45 +27,50 @@ void writecmd(command* cmd);
 void readcmd(command* cmd);
 void emptycmd(command* cmd);
 void printcmd(command* cmd);
+void worker(cmdqueue* queue);
+void enqueue(cmdqueue* queue, command* cmd);
+command* dequeue(cmdqueue* queue);
+
 void initfiles();
+int rng_sleep();
+int rng_empty();
 
-
+// global variables
+sem_t trial_lock;
 
 int main(){
+    // variable initializations
+    cmdqueue *queue = (cmdqueue*) malloc(sizeof(cmdqueue));
+    queue->head = NULL;
+    queue->tail = NULL;
+    sem_init(&trial_lock, 0, 1);
     char buffer[150];
     time_t timestamp;
 
     // file initializations
     initfiles();
     FILE *cmdtxt;
-    
+
     while(1){
         cmdtxt = fopen("commands.txt", "a");
         memset(buffer, '\0', 150);
         
-        printf("> ");
+        //printf("> ");
         fgets(buffer, 150, stdin);  // gets user input and stores it to buffer
         
         time(&timestamp);
 
         if((strcmp(buffer, "\n")) != 0){        // filters out empty inputs
             command* cmd = parsecmd(buffer);    // sends user input to parsecmd
-            pthread_t* worker = (pthread_t*) malloc (sizeof(pthread_t*));
+            fprintf(cmdtxt, "%s %s", ctime(&timestamp), buffer);
+            pthread_t* workerthr = (pthread_t*) malloc(sizeof(pthread_t));
+
+            //update queue
+            enqueue(queue, cmd);
 
             // sending command to worker threads
-            if(strcmp(cmd->type, "write") == 0){
-                fprintf(cmdtxt, "%s %s", ctime(&timestamp), buffer);
-                pthread_create(worker, NULL, (void *) writecmd, cmd);
-
-            } else if(strcmp(cmd->type, "read") == 0){
-                fprintf(cmdtxt, "%s %s", ctime(&timestamp), buffer);
-                pthread_create(worker, NULL, (void *) readcmd, cmd);
-
-            } else if(strcmp(cmd->type, "empty") == 0){
-                fprintf(cmdtxt, "%s %s", ctime(&timestamp), buffer);
-                pthread_create(worker, NULL, (void *) emptycmd, cmd);
-
-            } else printf("Command not found\n");
+            pthread_create(workerthr, NULL, (void *) worker, queue);
+            pthread_detach(*workerthr);
         }
 
         fclose(cmdtxt);
@@ -104,24 +117,21 @@ command* parsecmd(char buffer[150]){
 }
 
 void writecmd(command* cmd){
-    // initialize thread
-
-    // lock
     FILE *cmddir;
     cmddir = fopen(cmd->dir, "a");
+    int i = 0;
 
     //fputs(cmd->str, cmddir);
-    fprintf(cmddir, "%s\n", cmd->str);
-    fclose(cmddir);
+    while((cmd->str)[i] != '\0'){
+        sleep(0.025);
+        fprintf(cmddir, "%c", (cmd->str)[i]);
+        i++;
+    }
 
-    // unlock
+    fclose(cmddir);
 }
 
 void readcmd(command* cmd){
-    // initialize thread
-
-    // use read lock
-    
     FILE *readtxt, *cmddir;
     cmddir = fopen(cmd->dir, "r");
     readtxt = fopen("read.txt", "a"); // append to read.txt
@@ -140,15 +150,13 @@ void readcmd(command* cmd){
     }
 
     fclose(readtxt);
-
-    // unlock
 }
 
 void emptycmd(command* cmd){
     FILE *emptytxt, *cmddir;
     cmddir = fopen(cmd->dir, "r");
     emptytxt = fopen("empty.txt", "a"); // append to empty.txt
-    char rc; // read character variable
+    char rc;                            // read character variable
 
     if(cmddir){
         fprintf(emptytxt, "%s %s:\t", cmd->type, cmd->dir);
@@ -162,6 +170,7 @@ void emptycmd(command* cmd){
         //empty file contents
         cmddir = fopen(cmd->dir, "w");
         fclose(cmddir);
+        sleep(rng_empty());
     } else{
         fprintf(emptytxt, "%s %s:\tFILE ALREADY EMPTY\n", cmd->type, cmd->dir);
     }
@@ -169,8 +178,45 @@ void emptycmd(command* cmd){
     fclose(emptytxt);
 }
 
-void printcmd(command* cmd){
-    printf("%s\n%s\n%s\n", cmd->type, cmd->dir, cmd->str);
+void worker(cmdqueue* queue){
+    sleep(rng_sleep());
+    sem_wait(&trial_lock);
+    command* cmd = dequeue(queue);
+    
+    if(cmd != NULL){
+        if(strcmp(cmd->type, "write") == 0){
+            writecmd(cmd);
+
+        } else if(strcmp(cmd->type, "read") == 0){
+            readcmd(cmd);
+
+        } else if(strcmp(cmd->type, "empty") == 0){
+            emptycmd(cmd);
+
+        } else printf("Command not found\n");
+
+        free(cmd);
+    }
+    sem_post(&trial_lock);
+}
+
+void enqueue(cmdqueue* queue, command* cmd){
+    if(queue->head == NULL){
+        queue->head = cmd;
+        queue->tail = cmd;
+        cmd->nextcmd = NULL;
+    } else{
+        cmd->nextcmd = NULL;
+        queue->tail->nextcmd = cmd;
+        queue->tail = cmd;
+    }
+
+}
+
+command* dequeue(cmdqueue* queue){
+    command *cmd = queue->head;
+    if(cmd != NULL) queue->head = cmd->nextcmd;
+    return cmd;
 }
 
 void initfiles(){
@@ -183,3 +229,23 @@ void initfiles(){
     fclose(initreadtxt);
     fclose(initemptytxt);
 }
+
+int rng_sleep(){
+    srand(time(0));
+	if(rand()%100 >= 80){
+        return 6; // return 6 20% of the time
+    }
+    return 1;
+}
+
+int rng_empty(){
+    srand(time(0));
+	return (7 + (rand()%4)); // return a random integer between 7 and 10
+}
+
+/* 
+remaining tasks:
+1. fix timestamp dapat one line
+2. sscanf to strtok
+3. locks
+*/
